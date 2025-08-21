76706d3405f0db1ede52f1a72485a801c51b16a8
Anderson Gabriel <andersong.pereiracruz@gmail.com>
2025-08-21 15:24:04 -0300

    feat: adicionando função de verificar senha

--- backend/utils/criptografar.py
+++ backend/utils/criptografar.py
@@ -5,6 +5,9 @@ import base64
 def hash_senha(senha: str) -> str:
     return pwd_context.hash(senha)
 
+def verificar_senha(senha_fornecida: str, senha_hash: str) -> bool:
+    return pwd_context.verify(senha_fornecida, senha_hash)
+
 def criptografar_email(email: str) -> str:
     return base64.b64encode(email.encode()).decode()
 
