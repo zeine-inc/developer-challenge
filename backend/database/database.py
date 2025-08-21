3ba4ff5fc049a68cdf9ac0e2b22f57efcccdac11
Anderson Gabriel <andersong.pereiracruz@gmail.com>
2025-08-21 16:55:59 -0300

    chore: removendo comentários iniciais

--- backend/database/database.py
+++ backend/database/database.py
@@ -3,20 +3,16 @@ from sqlalchemy.orm import declarative_base, relationship
 from os import getenv
 from dotenv import load_dotenv
 
-# Carregamento das variáveis do .env
 load_dotenv()
 
-# Classes mapeadas para as tabelas
 Base = declarative_base()
 
-# Classe que compreende a entidade intermediária
 vendedor_contato = Table(
     'vendedor_contato', Base.metadata,
     Column('id_vendedor', Integer, ForeignKey('vendedor.id'), primary_key=True),
     Column('id_contato', Integer, ForeignKey('contato.id'), primary_key=True)
 )
 
-# Classe que compreende a entidade Vendedor
 class Vendedor(Base):
     __tablename__ = 'vendedor'
     id = Column(Integer, primary_key=True)
@@ -25,7 +21,6 @@ class Vendedor(Base):
     senha = Column(String(255), nullable=False)
     contatos = relationship('Contato', secondary=vendedor_contato, back_populates='vendedores')
 
-# Classe que compreende a entidade Contato
 class Contato(Base):
     __tablename__ = 'contato'
     id = Column(Integer, primary_key=True)
@@ -35,6 +30,5 @@ class Contato(Base):
     foto = Column(String(255))
     vendedores = relationship('Vendedor', secondary=vendedor_contato, back_populates='contatos')
 
-# Criar tabelas
 engine = create_engine(getenv("DATABASE_URL"))
 Base.metadata.create_all(engine)
