92679ef4d3fa075414beb6605a3bca6b377097d8
Anderson Gabriel <andersong.pereiracruz@gmail.com>
2025-08-21 19:23:18 -0300

    feat: criando componente de 'Button'

---
+++ frontend/src/components/button.tsx
@@ -0,0 +1,48 @@
+import { type ButtonHTMLAttributes } from "react";
+
+export enum ButtonVariant {
+  Primary = "primary",
+  Secondary = "secondary",
+  Tertiary = "tertiary",
+}
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  label: string;
+  variant?: ButtonVariant;
+}
+
+export default function Button({
+  label,
+  variant = ButtonVariant.Primary,
+  onClick,
+  ...props
+}: ButtonProps) {
+  const baseClasses =
+    "px-4 py-2 rounded-2xl font-medium transition-colors duration-200";
+
+  let variantClasses = "";
+
+  if (variant === ButtonVariant.Primary) {
+    variantClasses =
+      "bg-[var(--brand)] text-[var(--inverse)] hover:bg-[var(--brand)]";
+  }
+
+  if (variant === ButtonVariant.Secondary) {
+    variantClasses =
+      "bg-[var(--background-tertiary)] text-[var(--primary)] hover:bg-[var(--background-secondary)]";
+  }
+
+  if (variant === ButtonVariant.Tertiary) {
+    variantClasses =
+      "border border-[var(--border-primary)] bg-transparent text-[var(--primary)] hover:bg-[var(--background-tertiary)]";
+  }
+
+  return (
+    <button
+      onClick={onClick}
+      className={`${baseClasses} ${variantClasses}`}
+      {...props}>
+      {label}
+    </button>
+  );
+}
