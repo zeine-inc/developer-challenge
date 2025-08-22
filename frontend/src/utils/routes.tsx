52511c730b1a60e219e736bbee499900f98d8198
Anderson Gabriel <andersong.pereiracruz@gmail.com>
2025-08-22 10:55:47 -0300

    feat: definindo a rota da página inicial

--- frontend/src/utils/routes.tsx
+++ frontend/src/utils/routes.tsx
@@ -1,7 +1,9 @@
 import { PageLogin } from "../pages/login";
 import { PageCadastro } from "../pages/cadastro";
+import PageInicio from "../pages/inicio";
 
 export const myRoutes = [
   { path: "/", element: <PageLogin /> },
   { path: "/cadastro", element: <PageCadastro /> },
+  { path: "/inicio", element: <PageInicio /> },
 ];
