7f9cf960fb2cdd1e043d310f4ba484c1f8a0acee
Anderson Gabriel <andersong.pereiracruz@gmail.com>
2025-08-22 15:43:54 -0300

    feat: aplicando funcionalidade de manter filtro por ínicio do nome

--- frontend/src/components/filtrar.tsx
+++ frontend/src/components/filtrar.tsx
@@ -9,22 +9,30 @@ export default function FiltroAlfabeto({
   letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split(""),
   onSelecionar,
 }: FiltroAlfabetoProps) {
-  const [ativo, setAtivo] = useState<string | null>(null);
+  const [selecionada, setSelecionada] = useState<string | null>(null);
 
   const selecionar = (letra: string) => {
-    console.log(`Letra selecionar ${letra}`);
+    onSelecionar(letra);
+    setSelecionada(letra);
   };
 
   return (
     <div className="flex flex-col bg-[var(--brand)] rounded-[1.5rem] mt-[1rem] px-2 py-4 gap-2 items-center select-none w-[4rem]">
-      {letras.map((letra) => (
-        <button
-          key={letra}
-          onClick={() => selecionar(letra)}
-          className={`cursor-pointer text-[1.2rem] font-bold transition-colors text-[var(--muted)] hover:scale-150 hover:text-[var(--inverse)]`}>
-          {letra}
-        </button>
-      ))}
+      {letras.map((letra) => {
+        const isSelecionada = letra === selecionada;
+        return (
+          <button
+            key={letra}
+            onClick={() => selecionar(letra)}
+            className={`cursor-pointer font-bold transition-all ${
+              isSelecionada
+                ? "text-[1.5rem] text-[var(--inverse)]"
+                : "text-[1.2rem] text-[var(--muted)] hover:scale-150 hover:text-[var(--inverse)]"
+            }`}>
+            {letra}
+          </button>
+        );
+      })}
     </div>
   );
 }
--- frontend/src/pages/inicio.tsx
+++ frontend/src/pages/inicio.tsx
@@ -1,3 +1,111 @@
+import { useState } from "react";
+
+import Lupa from "../../public/icons/search.png";
+import Cadeado from "../../public/icons/lock.png";
+
+import type { Contato } from "../interfaces/interfaces";
+
+import SideBar from "../components/sidebar";
+import TextField from "../components/text_field";
+import Button, { ButtonVariant } from "../components/button";
+import IconButton from "../components/iconbutton";
+import FiltroAlfabeto from "../components/filtrar";
+import TabelaContatos from "../components/tabela_contatos";
+import ModalInformacoes from "../components/modalInformacoes";
+import ModalContato from "../components/modalContato";
+
 export default function PageInicio() {
-  return <></>;
+  const [busca, setBusca] = useState<string>("");
+  const [letraFiltar, setLetraFiltrar] = useState<string>("");
+  const [mostrarModalInformacoes, setMostrarModalInformacoes] =
+    useState<boolean>(false);
+  const [mostrarModalContato, setMostrarModalContato] =
+    useState<boolean>(false);
+  const [contatoSelecionado, setContatoSelecionado] = useState<
+    Contato | undefined
+  >();
+  const [tipoModal, setTipoModal] = useState<string>("add");
+
+  return (
+    <section aria-label="Página de contatos" className="flex">
+      <SideBar />
+      <div className="mt-[8rem] bg-[var(--background-secondary)] w-[80%] h-[80vh] rounded-[3rem]">
+        <div
+          aria-label="Seção do cabeçalho da página"
+          className="flex items-center justify-between gap-4 p-4 rounded-lg mt-[2rem]">
+          {/* Título */}
+          <h2 className="ml-[5rem] text-[2rem] font-bold text-white">
+            Lista de contatos
+          </h2>
+
+          <div className="flex gap-[1rem] p-[1rem]">
+            {/* Campo de busca */}
+            <div className="flex-1 w-[25rem]">
+              <TextField
+                label=""
+                input={busca}
+                onChange={setBusca}
+                type="text"
+                placeholder="Pesquisar"
+                icon={Lupa}
+              />
+            </div>
+
+            {/* Botão adicionar */}
+            <Button
+              label="+ Adicionar contato"
+              variant={ButtonVariant.Secondary}
+              onClick={() => {
+                setMostrarModalContato(true);
+                setTipoModal("add");
+              }}
+            />
+
+            <IconButton
+              imagem={Cadeado}
+              ativo={false}
+              onClick={() => setMostrarModalInformacoes(true)}
+            />
+          </div>
+        </div>
+        <div className="flex px-[4rem] w-full">
+          <FiltroAlfabeto
+            letras={"ABCDEFGHIJKLMNO".split("")}
+            onSelecionar={setLetraFiltrar}
+          />
+
+          <div aria-label="Sessão de Contatos" className="flex flex-col w-full">
+            {/* Cabeçalho do filtro */}
+            <div aria-label="Resultado do filtro" className="p-[2rem]">
+              <p className="font-bold text-[0.875rem] text-[var(--primary)]">
+                {letraFiltar}
+              </p>
+              <hr className="h-px w-[102%] my-4 bg-[var(--primary)] border-0" />
+            </div>
+
+            <TabelaContatos
+              onEditar={(contato) => {
+                if (contato != null) {
+                  setTipoModal("edit");
+                  setContatoSelecionado(contato);
+                  setMostrarModalContato(true);
+                }
+              }}
+            />
+          </div>
+        </div>
+      </div>
+
+      {mostrarModalInformacoes && (
+        <ModalInformacoes onClose={() => setMostrarModalInformacoes(false)} />
+      )}
+      {mostrarModalContato && (
+        <ModalContato
+          tipo={tipoModal}
+          contato={contatoSelecionado}
+          onClose={() => setMostrarModalContato(false)}
+        />
+      )}
+    </section>
+  );
 }
