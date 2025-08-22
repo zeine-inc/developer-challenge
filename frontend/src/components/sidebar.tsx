3c0bd1885fb27637aec17b35af7722d1beb52b05
Anderson Gabriel <andersong.pereiracruz@gmail.com>
2025-08-22 12:16:13 -0300

    feat: criando componente para SideBar

---
+++ frontend/src/components/sidebar.tsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import IconButton from "../components/iconbutton";
+
+import AccountActive from "../../public/icons/acconunt_active.png";
+import SettingActive from "../../public/icons/settings_active.png";
+import LogoutActive from "../../public/icons/logout_active.png";
+import Logo from "../../public/icons/logo.png";
+
+export default function SideBar() {
+  const acoesIcones = [
+    { id: 0, imagem: AccountActive },
+    { id: 1, imagem: SettingActive },
+    { id: 2, imagem: LogoutActive },
+  ];
+
+  // Inicializa ativo com o ID do primeiro botão
+  const [ativo, setAtivo] = useState<number>(0);
+
+  return (
+    <section
+      aria-label="Menu de Opções"
+      className="h-[100vh] flex flex-col justify-between p-[5rem] items-center">
+      <img src={Logo} className="w-[1.81rem] h-[1.98rem]" />
+      <div className="flex flex-col gap-[1rem] w-max text-center">
+        {acoesIcones.map((acao) => (
+          <IconButton
+            key={acao.id}
+            imagem={acao.imagem}
+            ativo={ativo === acao.id} // compara com o estado
+            onClick={() => setAtivo(acao.id)}
+          />
+        ))}
+      </div>
+      <div>
+        <p className="text-[0.875rem] text-[var(--muted)]">Logado como:</p>
+        <p className="text-[0.875rem] text-[var(--body)]">
+          {localStorage.getItem("email")}
+        </p>
+      </div>
+    </section>
+  );
+}
