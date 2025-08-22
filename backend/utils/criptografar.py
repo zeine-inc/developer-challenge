75df8994c6663a79092131fc7c69531826d17dd2
Anderson Gabriel <andersong.pereiracruz@gmail.com>
2025-08-22 17:23:29 -0300

    feat: adicionando funções de criptografia de telefone

--- backend/utils/criptografar.py
+++ backend/utils/criptografar.py
@@ -11,5 +11,14 @@ def verificar_senha(senha_fornecida: str, senha_hash: str) -> bool:
 def criptografar_email(email: str) -> str:
     return base64.b64encode(email.encode()).decode()
 
+def descriptografar_email(email_codificado: str) -> str:
+    return base64.b64decode(email_codificado.encode()).decode()
+
+def criptografar_telefone(telefone: str) -> str:
+    return base64.b64encode(telefone.encode()).decode()
+
+def descriptografar_telefone(telefone_codificado: str) -> str:
+    return base64.b64decode(telefone_codificado.encode()).decode()
+
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
