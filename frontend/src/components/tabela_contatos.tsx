c0213877fa50aaf7ab8c65739d795c34ffc501be
Anderson Gabriel <andersong.pereiracruz@gmail.com>
2025-08-22 13:32:27 -0300

    feat: criando tabela de contatos

---
+++ frontend/src/components/tabela_contatos.tsx
@@ -0,0 +1,106 @@
+import Button, { ButtonVariant } from "../components/button";
+import IconButton from "../components/iconbutton";
+
+import CadeadoAberto from "../../public/icons/lock_open.png";
+import Lixeira from "../../public/icons/remove.png";
+import Carmem from "../../public/images/Illustra-1.png";
+import Cristina from "../../public/images/Illustra.png";
+
+export default function TabelaContatos() {
+  const contatos = [
+    {
+      imagem: Carmem,
+      nome: "Carmem Lúcia",
+      relacao: "Trabalho",
+      contato: "(16) 3537-7333",
+      email: "carmem.lucia@example.com",
+    },
+    {
+      imagem: Cristina,
+      nome: "Cristina Silveira",
+      relacao: "Colega",
+      contato: "(19) 2337-5664",
+      email: "cristinasilveira98@example.com",
+    },
+  ];
+  return (
+    <table className="w-full ml-8 border-separate border-spacing-0">
+      <thead>
+        <tr className="text-left">
+          <th className="text-[var(--muted)] text-[0.75rem] font-bold w-[40%] pb-4">
+            NOME
+          </th>
+          <th className="text-[var(--muted)] text-[0.75rem] font-bold w-[20%] pb-4">
+            TELEFONE
+          </th>
+          <th className="text-[var(--muted)] text-[0.75rem] font-bold w-[25%] pb-4">
+            EMAIL
+          </th>
+          <th className="w-[15%] pb-4"></th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {contatos.map((contato, idx) => (
+          <>
+            <tr key={contato.email} className="align-middle">
+              {/* Nome + Imagem */}
+              <td className="py-8">
+                <div className="flex items-center gap-3">
+                  <img
+                    src={contato.imagem}
+                    className="w-[3rem] h-[3rem] rounded-xl"
+                  />
+                  <div>
+                    <p className="text-[var(--body)] text-[0.875rem] font-medium">
+                      {contato.nome}
+                    </p>
+                    <p className="text-[var(--muted)] text-[0.75rem]">
+                      {contato.relacao}
+                    </p>
+                  </div>
+                </div>
+              </td>
+
+              {/* Telefone */}
+              <td className="text-[var(--body)] text-[0.875rem] py-4">
+                {contato.contato}
+              </td>
+
+              {/* Email */}
+              <td className="text-[var(--body)] text-[0.875rem] py-4">
+                {contato.email}
+              </td>
+
+              {/* Ações */}
+              <td className="py-4">
+                <div className="flex gap-2">
+                  <Button label="Editar" variant={ButtonVariant.Secondary} />
+                  <IconButton
+                    imagem={CadeadoAberto}
+                    ativo={false}
+                    onClick={() => {}}
+                  />
+                  <IconButton
+                    imagem={Lixeira}
+                    ativo={false}
+                    onClick={() => {}}
+                  />
+                </div>
+              </td>
+            </tr>
+
+            {/* Linha separadora */}
+            {idx < contatos.length - 1 && (
+              <tr>
+                <td colSpan={4} className="px-2">
+                  <hr className="h-px w-[98%] bg-[var(--muted)] border-0" />
+                </td>
+              </tr>
+            )}
+          </>
+        ))}
+      </tbody>
+    </table>
+  );
+}
