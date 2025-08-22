8b94057cb87791c67cd307798adb2f657c71468c
Anderson Gabriel <andersong.pereiracruz@gmail.com>
2025-08-22 17:23:01 -0300

    fix: ajuste de 'relationShip' em relação N:N

--- backend/database/database.py
+++ backend/database/database.py
@@ -7,12 +7,11 @@ load_dotenv()
 
 Base = declarative_base()
 
-vendedor_contato = Table(
-    'vendedor_contato', Base.metadata,
-    Column('id_vendedor', Integer, ForeignKey('vendedor.id'), primary_key=True),
-    Column('id_contato', Integer, ForeignKey('contato.id'), primary_key=True),
-    Column('relacao', String(255))
-)
+class VendedorContato(Base):
+    __tablename__ = "vendedor_contato"
+    id_vendedor = Column(Integer, ForeignKey("vendedor.id"), primary_key=True)
+    id_contato = Column(Integer, ForeignKey("contato.id"), primary_key=True)
+    relacao = Column(String(255))
 
 class Vendedor(Base):
     __tablename__ = 'vendedor'
@@ -20,7 +19,6 @@ class Vendedor(Base):
     nome = Column(String(255), nullable=False)
     email = Column(String(255), nullable=False, unique=True)
     senha = Column(String(255), nullable=False)
-    contatos = relationship('Contato', secondary=vendedor_contato, back_populates='vendedores')
 
 class Contato(Base):
     __tablename__ = 'contato'
@@ -29,7 +27,6 @@ class Contato(Base):
     email = Column(String(255), unique=True)
     telefone = Column(String(255), unique=True)
     foto = Column(String(255))
-    vendedores = relationship('Vendedor', secondary=vendedor_contato, back_populates='contatos')
 
 engine = create_engine(getenv("DATABASE_URL"))
 Base.metadata.create_all(engine)
