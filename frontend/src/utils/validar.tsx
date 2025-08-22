53eea58b280f25a7643fba3e483c899cb824e5a2
Anderson Gabriel <andersong.pereiracruz@gmail.com>
2025-08-22 08:43:06 -0300

    feat: criação de função para validar senha via regex

--- frontend/src/utils/validar.tsx
+++ frontend/src/utils/validar.tsx
@@ -2,3 +2,8 @@ export function emailValido(email: string) {
   const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
   return emailRegex.test(email);
 }
+
+export function validarSenha(senha: string) {
+  const senhaRegex = /^(?=.*[\d\W]).{8,}$/;
+  return senhaRegex.test(senha);
+}
